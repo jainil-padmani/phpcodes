2b3849e85abc436d13e308f37d4b183a33f1146b
jainil-padmani <jainilpadmani99@gmail.com>
2024-05-31 13:07:39 +0530

    echo statement page addes

---
+++ 1 echo statement.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Hello <?php echo "WORLD!, "?> I am Jainil Padmani...</p> <!-- this way we can integrate php in between html  -->
+    <!-- Output will be "Hello World!, I am Jainil Padmani" -->
+    <?php 
+
+    // this is single line comment in php 
+    
+    echo "This is my First Page ..... ";  // must ensure that there is ";" at the end of statement
+
+    /*
+
+    this is multi line 
+    comment in php 
+
+    */
+    ?>
+    
+</body>
+</html>
